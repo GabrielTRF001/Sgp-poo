496b589ae4aa9bc103d91f1bd87ae2ffd94e2f5b
Gabriel <Testezin0028@gmail.com>
2026-05-09 10:10:31 -0300

    Segundo Comiit

---
+++ entidades/Projeto.java
@@ -0,0 +1,59 @@
+package entidades;
+
+import java.time.LocalDate;
+
+public class Projeto {
+    private long id;
+    private String nome;
+    private String descricao;
+    private LocalDate datainicio;
+    private LocalDate dataConclusao;
+    private Usuario responsavel;
+    public Projeto(long id, String nome, String descricao, LocalDate datainicio, LocalDate dataConclusao,
+            Usuario responsavel) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.datainicio = datainicio;
+        this.dataConclusao = dataConclusao;
+        this.responsavel = responsavel;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getDatainicio() {
+        return datainicio;
+    }
+    public void setDatainicio(LocalDate datainicio) {
+        this.datainicio = datainicio;
+    }
+    public LocalDate getDataConclusao() {
+        return dataConclusao;
+    }
+    public void setDataConclusao(LocalDate dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+    public Usuario getResponsavel() {
+        return responsavel;
+    }
+    public void setResponsavel(Usuario responsavel) {
+        this.responsavel = responsavel;
+    }
+    
+    
+}
---
+++ entidades/Tarefas.java
@@ -0,0 +1,86 @@
+import enuns.PrioridadeTarefa.java;
+
+import java.time.LocalDate;
+
+import enums.PrioridadeTarefa;
+import enuns.StatusTarefa;
+public class Tarefas {
+    private long id;
+    private String titulo;
+    private String descricao;
+    private LocalDate dataCriacao;
+    private LocalDate dataConclusao;
+    private PrioridadeTarefa prioridade;
+    private StatusTarefa status;
+    private Usuario usuario;
+    private Projeto projeto;
+    public Tarefas(){
+    }
+    public Tarefas(long id, String titulo, String descricao, LocalDate dataCriacao, LocalDate dataConclusao,
+            PrioridadeTarefa prioridade, StatusTarefa status, Usuario usuario, Projeto projeto) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+        this.dataConclusao = dataConclusao;
+        this.prioridade = prioridade;
+        this.status = status;
+        this.usuario = usuario;
+        this.projeto = projeto;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+    public LocalDate getDataConclusao() {
+        return dataConclusao;
+    }
+    public void setDataConclusao(LocalDate dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+    public PrioridadeTarefa getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(PrioridadeTarefa prioridade) {
+        this.prioridade = prioridade;
+    }
+    public StatusTarefa getStatus() {
+        return status;
+    }
+    public void setStatus(StatusTarefa status) {
+        this.status = status;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public Projeto getProjeto() {
+        return projeto;
+    }
+    public void setProjeto(Projeto projeto) {
+        this.projeto = projeto;
+    }
+    
+}
